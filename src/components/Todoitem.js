ca1f54d14f7e81c28f1e59bcba9ea9483c13c6af
pjachimowski <p.jachimowski89@gmail.com>
2020-10-27 19:27:07 +0100

    first commit

---
+++ src/components/Todoitem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Todoitem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Todoitem
